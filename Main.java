a6f20f3fe3915a8ac572f20676aee97c8a9394a7
María José <118678970+tismajo@users.noreply.github.com>
2024-01-24 07:45:23 -0600

    CSV generator and random numbers generator

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception{
+        generateCsv csv = new generateCsv();
+        csv.creacionCSV();
+    }
+}
---
+++ generateCsv.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class generateCsv {
+    public void creacionCSV() {
+        String csvFile = "random_numbers.csv";
+        try {
+            FileWriter writer = new FileWriter(csvFile);
+            Random random = new Random();
+            for (int i = 0; i < 3000; i++) {
+                int randomNumber = random.nextInt(1000); 
+                writer.append(String.valueOf(randomNumber));
+                writer.append("\n");
+            }
+            writer.flush();
+            writer.close();
+            System.out.println("Archivo CSV creado exitosamente!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
